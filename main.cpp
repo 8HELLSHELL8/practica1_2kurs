cb073b823e0fd071a28216e185e5518dfd359e61
Anton P <anton.mgdn@gmail.com>
2024-10-12 00:50:06 +0700

    starting point

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <sstream>
 #include "cjson/cJSON.h"
 using namespace std;
+
+string readJSON(const string& fileName)
+{
+    fstream file(fileName);
+    if (!file.is_open())
+    {
+        cerr << "Error reading " << fileName << ".json file!" << endl;
+        return "";
+    }
+
+    stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    return buffer.str();
+}
+
+bool createDir()
+{
+    if (filesystem::create_directory("123")) return true;
+    else return false;
+}
 
 
 int main()
 {
 
-
+    createDir();
     return 0;
 }
